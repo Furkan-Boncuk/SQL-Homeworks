f31009b8baba30753558cc8f7d2773fbbecd1ef6
Furkan Boncuk <114020260+Furkan-Boncuk@users.noreply.github.com>
2023-02-17 15:27:42 +0300

    Update Union.sql

--- Dersteki-Örnekler/Union.sql
+++ Dersteki-Örnekler/Union.sql
@@ -1,3 +1,4 @@
+------------------------------------------UNION---------------------------------------------
 select * from book order by page_number desc limit 5;
 
 select * from book order by title asc limit 5;
@@ -22,4 +23,25 @@ UNION
 (select id, email from author order by author.id asc)
 UNION
 (select id, title from book order by book.id desc) ;
+
+------------------------------------------INTERSECT------------------------------------------
+--INTERSECT operatörü iki farklı sorgunun kesişimini bulur.
+
+(select * from book order by page_number desc limit 10)
+INTERSECT 
+(select * from book order by title asc limit 10);
+
+------------------------------------------EXCEPT---------------------------------------------
+--1.sorguda olan ancak 2.sorguda olmayan verileri sıralar
+
+(select * from book order by page_number desc limit 10)
+EXCEPT 
+(select * from book order by title asc limit 10);
+
+--not: UNION'da olduğu gibi INTERSECT ve EXCEPT operatörlerinde de veri tipleri ve sütun sayıları uyuşmalıdır
+(select title from book order by page_number desc limit 10)
+EXCEPT 
+(select id from book order by title asc limit 10);
+--hata verir
+
 
