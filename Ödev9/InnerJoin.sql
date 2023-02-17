370d97e7c4413bca9b8dffa3bf1aa15980317b0a
Furkan Boncuk <114020260+Furkan-Boncuk@users.noreply.github.com>
2023-02-17 09:06:01 +0300

    Update InnerJoin.sql

--- Ödev9/InnerJoin.sql
+++ Ödev9/InnerJoin.sql
@@ -11,6 +11,5 @@ inner join payment on payment.customer_id = customer.customer_id order by paymen
 
 --3.customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve
 --last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
-
 select rental.rental_id, customer.first_name, customer.last_name from rental 
 inner join customer on rental.customer_id = customer.customer_id order by rental_id asc;
